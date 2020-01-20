7b2e5d6a459581eee2d658a2b09300d8c3369e47
unknown <ep.peoximasoft.net>
2020-01-20 17:22:59 +0600

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>to-do list</title>
+        <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}">
+        <script src="{{ asset('js/jquery.min.js') }}"></script>
+        <script src="{{ asset('js/bootstrap.min.js') }}"></script>
+    </head>
+    <body>
+    <section class="container">
+        <div class="row">
+            <div class="col-sm-12 col-md-6 mx-auto">
+                
+            </div>
+        </div>
+    </section>
+    </body>
+</html>
